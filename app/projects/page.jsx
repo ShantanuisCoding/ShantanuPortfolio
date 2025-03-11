04704b1f3fde01b6faafb9223c2233da0906023d
ShantanuisCoding <sshantanu781@gmail.com>
2025-03-11 11:36:27 +0530

    Projects DONE

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,26 +1,253 @@
+// "use client";
+
+// import { motion } from "framer-motion";
+// import React, { useState } from "react";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper/css";
+
+// import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
+// import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+
+// import {
+//   Tooltip,
+//   TooltipContent,
+//   TooltipProvider,
+//   TooltipTrigger,
+// } from "@/components/ui/tooltip";
+
+// import Link from "next/link";
+// import Image from "next/image";
+// import ProjectSliderBtns from "@/components/ProjectSliderBtns";
+
+
+// const projects = [
+
+//   {
+//     num: '01',
+//     category: 'WEB',
+//     title: "Search Engine using GoLang",
+//     description: "A fast and efficient search engine built using GoLang with optimized search algorithms.",
+//     stack: [{ name: "GoLang" }, { name: "Search Algorithms" }, { name: "Backend" }],
+//     image: "/assets/work/thumb3.png", // Add image URL here
+//     github: "https://github.com/ShantanuisCoding/Search-Engine",
+//     live: ""
+//   },
+//   {
+//     num: '02',
+//     category: 'SECURITY',
+//     title: "DDoS Protection System for Cloud",
+//     description: "An intelligent system to detect and mitigate DDoS attacks on cloud platforms.",
+//     stack: [{ name: "Cybersecurity" }, { name: "AI" }, { name: "Cloud Security" }],
+//     image: "/assets/work/thumb3.png", // Add image URL here
+//     github: "https://github.com/ShantanuisCoding/DDoS-Protection-System-for-Cloud",
+//     live: ""
+//   },
+
+//   {
+//     num: '03',
+//     category: 'WEB',
+//     title: "My Portfolio",
+//     description: "A personal portfolio showcasing projects, skills, and achievements.",
+//     stack: [{ name: "React" }, { name: "Tailwinnd CSS" }, { name: "Next.js" }],
+//     image: "/assets/work/thumb3.png", // Add image URL here
+//     github: "https://github.com/ShantanuisCoding/Search-Engine",
+//     live: ""
+//   },
+
+//   {
+//     num: '04',
+//     category: 'AI',
+//     title: "EDMRS (Emotion Detection Music Recommendation System)",
+//     description: "An AI-based music recommendation system that detects emotions and suggests songs accordingly.",
+//     stack: [{ name: "AI" }, { name: "Python" }, { name: "Machine Learning" }],
+//     image: "/assets/work/thumb3.png", // Add image URL here
+//     github: "https://github.com/ShantanuisCoding/EDMRS"
+//   },
+//   {
+//     num: '05',
+//     category: 'AI',
+//     title: "Video Translator",
+//     description: "An AI-powered video translation tool that translates speech and subtitles in real time.",
+//     stack: [{ name: "AI" }, { name: "Deep Learning" }, { name: "NLP" }],
+//     image: "/assets/work/thumb3.png", // Add image URL here
+//     github: "https://github.com/ShantanuisCoding/Video-Translator"
+//   },
+//   {
+//     num: '06',
+//     category: 'AI',
+//     title: "Voice Cloner",
+//     description: "A deep learning-based system for cloning voices with high accuracy and realism.",
+//     stack: [{ name: "Deep Learning" }, { name: "AI" }, { name: "Speech Processing" }],
+//     image: "/assets/work/thumb3.png", // Add image URL here
+//     github: "https://github.com/ShantanuisCoding/Voice-Cloner"
+//   },
+//   {
+//     num: '07',
+//     category: 'GAME',
+//     title: "Sudoku Solver",
+//     description: "An AI-powered Sudoku solver that provides instant solutions using backtracking algorithms.",
+//     stack: [{ name: "AI" }, { name: "Python" }, { name: "Algorithms" }],
+//     image: "/assets/work/thumb3.png", // Add image URL here
+//     github: "https://github.com/ShantanuisCoding/Sudoku-Solver"
+//   },
+// ]
+
+// const Projects = () => {
+//   const [project, setProject] = useState(projects[0]);
+
+//   const handelSlideChnge = (swiper) => {
+//     const currentIndex = swiper.activeIndex;
+//     setProject(projects[currentIndex]);
+//   };
+//   return (
+//     <motion.section
+//       initial={{ opacity: 0 }}
+//       animate={{
+//         opacity: 1,
+//         transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+//       }}
+//       className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0">
+//       <div className="container mx-auto ">
+//         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+//           <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+//             <div className="flex flex-col gap-[30px] h-[50%]">
+//               {/* outline num */}
+//               <div className="text-8xl leading-none font-extrabold text-transparent" style={{ WebkitTextStroke: "2px white", }}>
+//                 {project.num}
+//               </div>
+
+//               {/* project category */}
+
+//               {/* <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+//                 {project.category}
+//               </h2> */}
+//               {/* project title */}
+
+//               <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
+//                 {project.title}
+//               </h2>
+
+//               {/* project description */}
+//               <p className="text-white/60">
+//                 {project.description}
+//               </p>
+
+//               {/* techstack */}
+//               <ul className="flex gap-4">
+//                 {project.stack.map((item, index) => {
+//                   return (
+//                     <li key={index} className="text-xl text-accent">
+//                       {item.name}
+//                       {/* remove the last comma */}
+//                       {index !== project.stack.length - 1 && ","}
+//                     </li>
+//                   );
+//                 })}
+//               </ul>
+
+//               <div className="" >
+//                 {/* {buttons} */}
+
+//                 <div className="flex items-center gap-4">
+//                   {/* live button */}
+
+//                   {project.live && (
+//                     <Link href={project.live}>
+//                       <TooltipProvider delayDuration={100}>
+//                         <Tooltip>
+//                           <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+//                             <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
+//                           </TooltipTrigger>
+//                           <TooltipContent>
+//                             <p>Live Project</p>
+//                           </TooltipContent>
+//                         </Tooltip>
+//                       </TooltipProvider>
+//                     </Link>
+//                   )}
+
+//                   {/* github button */}
+
+//                   {project.github && (
+//                     <Link href={project.github}>
+//                       <TooltipProvider delayDuration={100}>
+//                         <Tooltip>
+//                           <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+//                             <BsGithub className="text-white text-3xl group-hover:text-accent" />
+//                           </TooltipTrigger>
+//                           <TooltipContent>
+//                             <p>Github Repository</p>
+//                           </TooltipContent>
+//                         </Tooltip>
+//                       </TooltipProvider>
+//                     </Link>
+//                   )}
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//           <div className="relative w-full xl:w-[50%]">
+//             <Swiper
+//               spaceBetween={30}
+//               slidesPerView={1}
+//               className="xl:h-[520px] mb-12"
+//               onSlideChange={handelSlideChnge}
+//             >
+//               {projects.map((project, index) => (
+//                 <SwiperSlide key={index} className="w-full">
+//                   <div className="h-[460px] relative flex justify-center items-center bg-pink-50/20">
+//                     {/* Overlay */}
+//                     <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+
+//                     {/* Image */}
+//                     <div className="relative w-full h-full">
+//                       <Image
+//                         src={project.image}
+//                         fill
+//                         className="object-cover"
+//                         alt=""
+//                       />
+//                     </div>
+//                   </div>
+//                 </SwiperSlide>
+//               ))}
+//             </Swiper>
+
+//             {/* Arrows ko image ke bahar shift kiya */}
+//             <button
+//               className="absolute left-0 top-1/2 -translate-y-1/2 z-50 bg-black hover:bg-accent text-white p-3 rounded-full border border-white"
+//               onClick={() => swiper.slidePrev()}
+//             >
+//               <PiCaretLeftBold className="text-3xl" />
+//             </button>
+
+//             <button
+//               className="absolute right-0 top-1/2 -translate-y-1/2 z-50 bg-black hover:bg-accent text-white p-3 rounded-full border border-white"
+//               onClick={() => swiper.slideNext()}
+//             >
+//               <PiCaretRightBold className="text-3xl" />
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </motion.section>
+//   );
+// };
+
+// export default Projects;
+
 "use client";
 
 import { motion } from "framer-motion";
 import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-
+import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
 import { BsArrowUpRight, BsGithub } from "react-icons/bs";
-
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-
 import Link from "next/link";
 import Image from "next/image";
-import  ProjectSliderBtns from "@/components/ProjectSliderBtns";
-
 
 const projects = [
-
   {
     num: '01',
     category: 'WEB',
@@ -95,117 +322,87 @@ const Projects = () => {
   const [project, setProject] = useState(projects[0]);
 
   const handelSlideChnge = (swiper) => {
-    const currentIndex = swiper.activeIndex;
-    setProject(projects[currentIndex]);
+    setProject(projects[swiper.activeIndex]);
   };
+
   return (
     <motion.section
       initial={{ opacity: 0 }}
-      animate={{
-        opacity: 1,
-        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
-      }}
-      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0">
-      <div className="container mx-auto ">
+      animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          {/* Left Content */}
           <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
             <div className="flex flex-col gap-[30px] h-[50%]">
-              {/* outline num */}
-              <div className="text-8xl leading-none font-extrabold text- transparent text-outline">
+              <div className="text-8xl leading-none font-extrabold text-transparent" style={{ WebkitTextStroke: "2px white" }}>
                 {project.num}
               </div>
-              {/* project category */}
-              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
-                {project.category} project
-              </h2>
-              {/* project description */}
-              <p className="text-white/60">
-                {project.description}
-              </p>
-              {/* techstack */}
-              <ul className="flex gap-4">
-                {project.stack.map((item, index) => {
-                  return (
-                    <li key={index} className="text-xl text-accent">
-                      {item.name}
-                      {/* remove the last comma */}
-                      {index !== project.stack.length - 1 && ","}
-                    </li>
-                  );
-                })}
-              </ul>
-              <div className="" >
-                {/* {buttons} */}
-                <div className="flex items-center gap-4">
-                  {/* live button */}
-                  <Link href={project.live}>
-                    <TooltipProvider delayDuration={100}>
-                      <Tooltip>
-                        <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                          <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <p>
-                            Live Project
-                          </p>
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
-                  </Link>
-                  {/* github button */}
-                  <Link href={project.github}>
-                    <TooltipProvider delayDuration={100}>
-                      <Tooltip>
-                        <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                          <BsGithub className="text-white text-3xl group-hover:text-accent" />
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <p>
-                            Github Repostitory
-                          </p>
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
-                  </Link>
-                </div>
 
+              <h2 className="text-[42px] font-bold leading-none text-white transition-all duration-500">
+                {project.title}
+              </h2>
+
+              <p className="text-white/60">{project.description}</p>
+
+              {/* Tech Stack */}
+              <ul className="flex gap-4">
+                {project.stack.map((item, index) => (
+                  <li key={index} className="text-xl text-accent">
+                    {item.name}
+                    {index !== project.stack.length - 1 && ","}
+                  </li>
+                ))}
+              </ul>
+
+              {/* Links */}
+              <div className="flex items-center gap-4">
+                {project.github && (
+                  <Link href={project.github}>
+                    <BsGithub className="text-white text-3xl hover:text-accent" />
+                  </Link>
+                )}
               </div>
             </div>
           </div>
-          <div className="w-full xl:w-[50%] ">
+
+          {/* Swiper Slider */}
+          <div className="relative w-full xl:w-[50%]">
             <Swiper
               spaceBetween={30}
               slidesPerView={1}
-              className="xl-h-[520px] mb-12"
+              className="xl:h-[520px] mb-12"
               onSlideChange={handelSlideChnge}
             >
-              {projects.map((project, index) => {
-                return (
-                  <SwiperSlide key={index} className="w-full">
-                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
-                      {/* overlay */}
-                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10">
-                      </div>
-                      {/* Image */}
-                      <div className="relative w-full h-full">
-                        <Image
-                          src={project.image}
-                          fill
-                          className="object-cover"
-                          alt=""
-                        />
-                      </div>
+              {projects.map((project, index) => (
+                <SwiperSlide key={index} className="w-full">
+                  <div className="h-[460px] relative flex justify-center items-center bg-pink-50/20">
+                    <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+
+                    <div className="relative w-full h-full">
+                      <Image src={project.image} fill className="object-cover" alt="" />
                     </div>
-                  </SwiperSlide>
-                );
-              })}
-              {/* buttons */}
-              <ProjectSliderBtns containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px) xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none" 
-              btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all" />
+                  </div>
+                </SwiperSlide>
+              ))}
             </Swiper>
           </div>
         </div>
       </div>
+      <button
+        className="fixed left-5 top-1/2 -translate-y-1/2 z-50 bg-transparent text-white p-4 rounded-full hover:scale-150"
+        onClick={() => document.querySelector(".swiper")?.swiper?.slidePrev()}
+      >
+        <PiCaretLeftBold className="text-7xl" />
+      </button>
+
+      <button
+        className="fixed right-5 top-1/2 -translate-y-1/2 z-50 bg-transparent text-white p-4 rounded-full hover:scale-150"
+        onClick={() => document.querySelector(".swiper")?.swiper?.slideNext()}
+      >
+        <PiCaretRightBold className="text-7xl" />
+      </button>
     </motion.section>
   );
 };
--- components/ProjectSliderBtns.jsx
+++ components/ProjectSliderBtns.jsx
@@ -1,23 +1,75 @@
+// "use client";
+
+// import { useSwiper } from "swiper/react";
+// import "swiper/css";
+// import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
+
+// const ProjectSliderBtns = ({ containerStyles, btnStyles, iconStyles }) => {
+//     const swiper = useSwiper();
+
+//     if (!swiper) return null;
+
+//     return (
+//         <div className={containerStyles}>
+//             {/* Left Arrow */}
+//             <button
+//                 className="absolute left-0 top-1/2 -translate-y-1/2 z-50 bg-white/20 hover:bg-white/50 text-white p-4 rounded-full"
+//                 onClick={() => swiper.slidePrev()}
+//             >
+//                 <PiCaretLeftBold className="text-4xl" />
+//             </button>
+
+//             {/* Right Arrow */}
+//             <button
+//                 className="absolute right-0 top-1/2 -translate-y-1/2 z-50 bg-white/20 hover:bg-white/50 text-white p-4 rounded-full"
+//                 onClick={() => swiper.slideNext()}
+//             >
+//                 <PiCaretRightBold className="text-4xl" />
+//             </button>
+//         </div>
+//     );
+// };
+
+// export default ProjectSliderBtns;
+
+
+// <button className={btnStyles} onClick={() => swiper.slidePrev()}>
+//                 <PiCaretLeftBold className={iconStyles} />
+//             </button>
+//             <button className={btnStyles} onClick={() => swiper.slideNext()}>
+//                 <PiCaretRightBold className={iconStyles} />
+//             </button>
+
+
 "use client";
 
 import { useSwiper } from "swiper/react";
 import "swiper/css";
 import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
 
-const ProjectSliderBtns = ({ containerStyles, btnStyles, iconStyles }) => {
+const ProjectSliderBtns = () => {
     const swiper = useSwiper();
-    
-    if(!swiper) return null;
+
+    if (!swiper) return null;
 
     return (
-        <div className={containerStyles}>
-            <button className={btnStyles} onClick={() => swiper.slidePrev()}>
-                <PiCaretLeftBold className={iconStyles} />
+        <>
+            {/* Left Arrow - Extreme Left of the Page */}
+            <button
+                className="fixed left-5 top-1/2 -translate-y-1/2 z-50 bg-transparent hover:bg-accent text-white p-4 rounded-full  shadow-lg"
+                onClick={() => swiper.slidePrev()}
+            >
+                <PiCaretLeftBold className="text-4xl" />
             </button>
-            <button className={btnStyles} onClick={() => swiper.slideNext()}>
-                <PiCaretRightBold className={iconStyles} />
+
+            {/* Right Arrow - Extreme Right of the Page */}
+            <button
+                className="fixed right-5 top-1/2 -translate-y-1/2 z-50 bg-transparent hover:bg-accent text-white p-4 rounded-full shadow-lg"
+                onClick={() => swiper.slideNext()}
+            >
+                <PiCaretRightBold className="text-4xl" />
             </button>
-        </div>
+        </>
     );
 };
 
