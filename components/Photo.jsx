e042dd1b442f8c0c97a9c2de752db6135bc7563f
ShantanuisCoding <sshantanu781@gmail.com>
2025-04-06 15:06:39 +0530

    heheheh

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -39,7 +39,7 @@ const Photo = () => {
                 {/* Profile Image */}
                 <div className="relative w-[105%] max-w-[650px] p-4 rounded-full shadow-lg">
                     <Image
-                        src="/assets/photo.png"
+                        src="/assets/photo_main.png"
                         priority
                         quality={100}
                         width={350}
